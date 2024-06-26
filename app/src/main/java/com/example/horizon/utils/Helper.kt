67ddff22bae6e2ff9a403bee99c2ca45d66ce582
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2024-06-26 22:14:38 +0530

    Firebase Config Setup (#2)
    * Basic Setup
    
    * Firebase Config setup

--- app/src/main/java/com/example/horizon/utils/Helper.kt
+++ app/src/main/java/com/example/horizon/utils/Helper.kt
@@ -1,10 +1,22 @@
 package com.example.horizon.utils
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.util.Log
 import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.horizon.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
+
 
 class Helper {
     private lateinit var frameLayout: FrameLayout
@@ -28,4 +40,41 @@ class Helper {
     fun hideFrameLayout() {
         frameLayout.visibility = FrameLayout.INVISIBLE
     }
+    fun firebaseConfig(context: Context){
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        setupDefaultConfig(remoteConfig,context)
+    }
+    private fun setupDefaultConfig(remoteConfig: FirebaseRemoteConfig, context: Context ) {
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+        fetchConfig(remoteConfig,context)
+    }
+    private fun fetchConfig(remoteConfig: FirebaseRemoteConfig, context: Context) {
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(
+                context as Activity
+            ) { task ->
+            if(task.isSuccessful)
+            {
+                val updated = task.result
+                Log.d(TAG, "Config params updated: $updated")
+                Toast.makeText(
+                    context,
+                    "Fetch and activate succeeded",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                Toast.makeText(
+                    context,
+                    "Fetch failed",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+        }
+    }
 }
+
+
