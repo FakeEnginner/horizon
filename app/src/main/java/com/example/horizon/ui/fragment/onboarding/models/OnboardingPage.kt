c07892afa31f8b3c6abe1063bebbc8c260e24d4c
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2024-10-30 01:46:12 +0530

    remove unwanted res

--- app/src/main/java/com/example/horizon/ui/fragment/onboarding/models/OnboardingPage.kt
+++ app/src/main/java/com/example/horizon/ui/fragment/onboarding/models/OnboardingPage.kt
@@ -1,6 +1,6 @@
 package com.example.horizon.ui.fragment.onboarding.models
 
 data class OnboardingPage(
-    val imageResId: String,
+    val imageResId: Int,
     val description: String
 )
--- app/src/main/java/com/example/horizon/ui/fragment/onboarding/onboardingFragment.kt
+++ app/src/main/java/com/example/horizon/ui/fragment/onboarding/onboardingFragment.kt
@@ -6,12 +6,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.example.horizon.Interface.OnboardingDataListener
-import com.example.horizon.Interface.dao.onBoardingDao
 import com.example.horizon.MyApplication
 import com.example.horizon.R
 import com.example.horizon.databinding.FragmentOnboardingBinding
@@ -21,92 +19,85 @@ import com.example.horizon.ui.fragment.login.login
 import com.example.horizon.ui.fragment.onboarding.adapter.OnboardingAdapter
 import com.example.horizon.ui.fragment.onboarding.models.OnboardingPage
 import com.example.horizon.utils.Helper
-import com.example.horizon.utils.firebaseConfig
 import com.example.horizon.viewModel.OnBoardingCheckViewModel
-import com.google.gson.Gson
-import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 import org.json.JSONObject
 
-class onboardingFragment : Fragment(), OnboardingDataListener {
+class onboardingFragment() : Fragment(), OnboardingDataListener {
 
+    override val imageResourceMap: Map<String, Int> = mapOf(
+        "image1" to R.drawable.anxiety, // replace with actual drawable resource IDs
+        "image2" to R.drawable.yoga
+    )
+
+    private lateinit var binding: FragmentOnboardingBinding
     private lateinit var viewPager: ViewPager2
     private lateinit var onboardingAdapter: OnboardingAdapter
-    lateinit var onboardingBinding: FragmentOnboardingBinding
-    private lateinit var onBoardingCheckViewModel: OnBoardingCheckViewModel
-    private lateinit var onBoardingDao: onBoardingDao
-    var helper = Helper()
-    var firebaseConfig = firebaseConfig()
-     lateinit var dotsIndicator : DotsIndicator
-
-
+    private val helper = Helper()
+    private val onBoardingCheckViewModel: OnBoardingCheckViewModel by viewModels {
+        OnBoardingCheckViewModelFactory((requireActivity().application as MyApplication).database)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_onboarding, container, false)
-        return view
-
+    ): View {
+        binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        firebaseConfig.setOnboardingDataListener(this)
-        firebaseConfig.firebaseConfig(requireContext())
-        viewPager = view.findViewById(R.id.viewPager)
-        dotsIndicator = view.findViewById<DotsIndicator>(R.id.dots_indicator)
 
-        val application = requireActivity().application as MyApplication
-        val viewModelFactory = OnBoardingCheckViewModelFactory(application.database)
+        viewPager = binding.viewPager
+        val dotsIndicator = binding.dotsIndicator
 
-        onBoardingCheckViewModel = ViewModelProvider(this, viewModelFactory)
-            .get(OnBoardingCheckViewModel::class.java)
-
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-
+        // Sample JSON data for onboarding screens
+        val defaultConfig = """
+            {
+                "onboarding": [
+                    { "image": "image1", "description": "Welcome to the first page of onboarding." },
+                    { "image": "image2", "description": "Learn more about how to use our app." }
+                ]
             }
-        })
+        """.trimIndent()
 
-        view.findViewById<ImageView>(R.id.btnNext).setOnClickListener {
+        onBoardingDataReceived(defaultConfig)
+
+        // Next button functionality
+        binding.btnNext.setOnClickListener {
             val nextItem = viewPager.currentItem + 1
             if (nextItem < onboardingAdapter.itemCount) {
                 viewPager.setCurrentItem(nextItem, true)
-            } else {
             }
         }
-        view.findViewById<ImageView>(R.id.skipedbtn).setOnClickListener{
+
+        // Skip button functionality
+        binding.skipedbtn.setOnClickListener {
             val onBoardingCheck = onBoardingCheck(onBoardingCheck = true)
             onBoardingCheckViewModel.insertOnBoardingCheck(onBoardingCheck)
-            helper.replaceFragment(login(),requireFragmentManager())
+            helper.replaceFragment(login(), requireFragmentManager())
         }
     }
 
-
     override fun onBoardingDataReceived(jsonString: String) {
+        val onboardingPages = mutableListOf<OnboardingPage>()
+        val jsonObject = JSONObject(jsonString)
+        val jsonArray = jsonObject.getJSONArray("onboarding")
 
-        if (!jsonString.isNullOrBlank()) {
-            val gson = Gson()
-            val jsonObject = JSONObject(jsonString)
-            val jsonArray = jsonObject.getJSONArray("onboarding")
-            val onboardingPages = mutableListOf<OnboardingPage>()
+        for (i in 0 until jsonArray.length()) {
+            val pageObject = jsonArray.getJSONObject(i)
+            val imageId = pageObject.getString("image")
+            val description = pageObject.getString("description")
+            val imageResId = imageResourceMap[imageId] ?: R.drawable.yoga
 
-            for (i in 0 until jsonArray.length()) {
-                val jsonObject = jsonArray.getJSONObject(i)
-                val image = jsonObject.getString("image")
-                val description = jsonObject.getString("description")
-
-                // Create OnboardingPage object and add to list
-                val onboardingPage = OnboardingPage(image, description)
-                onboardingPages.add(onboardingPage)
-            }
-
-            onboardingAdapter = OnboardingAdapter(onboardingPages)
-            viewPager.adapter = onboardingAdapter
-            dotsIndicator.setViewPager2(viewPager)
+            val onboardingPage = OnboardingPage(imageResId, description)
+            onboardingPages.add(onboardingPage)
         }
 
+        onboardingAdapter = OnboardingAdapter(onboardingPages)
+        viewPager.adapter = onboardingAdapter
+        binding.dotsIndicator.setViewPager2(viewPager)
     }
 
     override fun onFetchFailed() {
