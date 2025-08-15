dc15538f7a8397556b8e9fbff6c4acada9df7168
Dakshendra Singh <61901342+daksh80@users.noreply.github.com>
2025-08-15 16:13:11 +0530

    Update .gitignore for interceptor/node_modules and untrack its contents

--- api/modules/globals.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    prependHttp (url) {
-        if (!url.startsWith('http://') && !url.startsWith('https://')) {
-            url = 'http://' + url;
-        }
-        return url;
-    }
-}
